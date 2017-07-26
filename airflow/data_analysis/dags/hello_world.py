8667297ab69495481f10b4621f55c3e0145bc23d
Massimo Menichinelli <info@openp2pdesign.org>
2017-07-26 10:10:11 +0200

    Improve the hello_world template

--- airflow/data_analysis/dags/hello_world.py
+++ airflow/data_analysis/dags/hello_world.py
@@ -1,30 +1,96 @@
-from datetime import datetime
+# -*- encoding: utf-8 -*-
+#
+# Template for Airflow DAGS
+#
+# Author: Massimo Menichinelli
+# E-mail: info@openp2pdesign.org - massimo@fablabbcn.org
+# License: MIT
+#
+#
+
+# Import various airflow modules
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
-#from airflow.models import Variable
-import requests
+# Load variables stored in Airflow
+from airflow.models import Variable
 
-import makerlabs
-import json
+# Work with time
+from datetime import datetime
 
-def get_data():
-    data = requests.get("https://api.fablabs.io/v0/labs.json").json()
-    return data
+# Store and retrieve data in Mongo database
+from pymongo import MongoClient
 
-#def give_feedback():
-#    username = Variable.get("github_user")
-#    return username
+# Example: load data from Fablabs.io
+from makerlabs import fablabs_io
 
 
-dag = DAG('hello_world', description='Simple tutorial DAG',
+# DAG name (for the DAG but also for the databases)
+dag_name = "hello_world"
+
+# Connect to Mongo database in the Docker compose
+client = MongoClient('mongodb://mongo:27017')
+# Create a database for this DAG
+db = client[dag_name]
+# Update the list of Meteor visualizations with this database
+db_meteor_list = client['meteor_visualizations_list']
+
+
+# Setup the Python functions for the operators
+
+# This function loads data and saves it into the raw collection
+def first_def():
+    data = fablabs_io.get_labs(format="dict")
+    # Save raw data in the raw collection
+    db.raw.insert_one(data)
+    return "Data saved successfully."
+
+# This function loads raw data, transforms it for the visualization
+# and update the list of visualizations in Meteor
+def second_def():
+    # Load data from the raw collection
+    data = db.raw.find_one({})
+    # Save meteor-ready data in the viz collection
+    db.viz.insert_one(data)
+    # Update the list of visualizations in Meteor
+    meteor_viz = db_meteor_list.vizs.find_one({"name": dag_name})
+    meteor_viz["type"] = "geo"
+    db_meteor_list.vizs.insert_one(meteor_viz)
+    return "Data prepared for the visualization successfully."
+
+
+# Setup the DAG
+#
+# schedule_interval uses the cron format
+#
+# * * * * * *
+# | | | | | | 
+# | | | | | +-- Year              (range: 1900-3000)
+# | | | | +---- Day of the Week   (range: 1-7, 1 standing for Monday)
+# | | | +------ Month of the Year (range: 1-12)
+# | | +-------- Day of the Month  (range: 1-31)
+# | +---------- Hour              (range: 0-23)
+# +------------ Minute            (range: 0-59)
+#
+# See more: http://www.nncron.ru/help/EN/working/cron-format.htm
+
+dag = DAG(dag_name,
+          description='Simple template for DAGs',
           schedule_interval='3 * * * *',
-          start_date=datetime(2017, 3, 20), catchup=False)
+          start_date=datetime(2017, 3, 20),
+          catchup=False)
 
-dummy_operator = DummyOperator(task_id='dummy_task', retries=3, dag=dag)
 
-get_operator = PythonOperator(task_id='hello_task', python_callable=get_data, dag=dag)
+# Setup the operators
+first_operator = PythonOperator(
+    task_id='hello_task_01',
+    python_callable=first_def,
+    dag=dag)
 
-#feedback_operator = PythonOperator(task_id='fdb_task', python_callable=give_feedback, dag=dag)
+second_operator = PythonOperator(
+    task_id='hello_task_02',
+    python_callable=second_def,
+    dag=dag)
 
-dummy_operator >> get_operator #>> feedback_operator
+
+# Setup the flow of operators
+first_operator >> second_operator
