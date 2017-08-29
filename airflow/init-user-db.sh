b3c0e204776fbad282e2168b1cb5d0f7cea814fd
Massimo Menichinelli <info@openp2pdesign.org>
2017-08-29 19:46:36 +0200

    Create a database for dag data in postgres at init time

--- airflow/init-user-db.sh
+++ airflow/init-user-db.sh
@@ -5,5 +5,7 @@ export PGUSER=postgres
 psql <<- EOSQL
     CREATE USER airflow;
     CREATE DATABASE airflow;
+    CREATE DATABASE airflow_dag_data;
     GRANT ALL PRIVILEGES ON DATABASE airflow TO airflow;
+    GRANT ALL PRIVILEGES ON DATABASE airflow_dag_data TO airflow;
 EOSQL
