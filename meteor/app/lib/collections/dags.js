8615b350e8a0583f632aae49995abc2aa0781461
Massimo Menichinelli <info@openp2pdesign.org>
2017-10-24 14:31:32 +0200

    Improve meteor structure

--- meteor/app/lib/collections/dags.js
+++ meteor/app/lib/collections/dags.js
@@ -6,79 +6,113 @@
 Dags = new Mongo.Collection('dags');
 
 if (Meteor.isServer) {
-  Dags.allow({
-    insert: function (userId, doc) {
-      return false;
-    },
+    Dags.allow({
+        insert: function(userId, doc) {
+            return false;
+        },
 
-    update: function (userId, doc, fieldNames, modifier) {
-      return false;
-    },
+        update: function(userId, doc, fieldNames, modifier) {
+            return false;
+        },
 
-    remove: function (userId, doc) {
-      return false;
-    }
-  });
+        remove: function(userId, doc) {
+            return false;
+        }
+    });
 
-  Dags.deny({
-    insert: function (userId, doc) {
-      return true;
-    },
+    Dags.deny({
+        insert: function(userId, doc) {
+            return true;
+        },
 
-    update: function (userId, doc, fieldNames, modifier) {
-      return true;
-    },
+        update: function(userId, doc, fieldNames, modifier) {
+            return true;
+        },
 
-    remove: function (userId, doc) {
-      return true;
-    }
-  });
+        remove: function(userId, doc) {
+            return true;
+        }
+    });
 }
 
 
 // Meteor visualisations based on the Airflow DAGs
-DagsVis = new Mongo.Collection('dags');
+DagsVis = new Mongo.Collection('dagsvis');
 
 if (Meteor.isServer) {
-  DagsVis.allow({
-    insert: function (userId, doc) {
-      return false;
-    },
+    DagsVis.allow({
+        insert: function(userId, doc) {
+            return false;
+        },
 
-    update: function (userId, doc, fieldNames, modifier) {
-      return false;
-    },
+        update: function(userId, doc, fieldNames, modifier) {
+            return false;
+        },
 
-    remove: function (userId, doc) {
-      return false;
-    }
-  });
+        remove: function(userId, doc) {
+            return false;
+        }
+    });
 
-  DagsVis.deny({
-    insert: function (userId, doc) {
-      return true;
-    },
+    DagsVis.deny({
+        insert: function(userId, doc) {
+            return true;
+        },
 
-    update: function (userId, doc, fieldNames, modifier) {
-      return true;
-    },
+        update: function(userId, doc, fieldNames, modifier) {
+            return true;
+        },
 
-    remove: function (userId, doc) {
-      return true;
-    }
-  });
+        remove: function(userId, doc) {
+            return true;
+        }
+    });
 }
 
 // Listen for changes in the Airflow DAGs collections in order manage visualisations
 var cursor = Dags.find();
 cursor.observeChanges({
     added: function(id, fields) {
-        console.log("ADDED:", id);
+        // A new data source from DAGs was added
+        console.log("A new DAGs data source was created in Mongo with id:", id._str);
+        // Create a visualisation
+        Meteor.call('add_vis', id, function(error, result) {
+            if (error) {
+                console.log("Error in adding a visualisation:", error, "ID:", id._str);
+            } else {
+                console.log("A new DAGs data vis was created with id:", result);
+                Router.go('vis', {
+                    _id: result
+                });
+            }
+        });
     },
     changed: function(id, fields) {
-        console.log("CHANGED:", id);
+        // An existing data source from DAGs was edited
+        console.log("An existing DAGs data source was edited in Mongo with id:", id._str);
+        // Edit a visualisation
+        Meteor.call('change_vis', id, function(error, result) {
+            if (error) {
+                console.log("Error in editing a visualisation:", error, "ID:", id._str);
+            } else {
+                console.log("An existing DAGs data vis was edited with id:", result);
+                Router.go('vis', {
+                    _id: result
+                });
+            }
+        });
     },
     removed: function(id) {
-        console.log("REMOVED:", id);
+        // An existing data source from DAGs was deleted
+        console.log("An existing DAGs data source was deleted in Mongo with id:", id._str);
+        // Delete a visualisation
+        Meteor.call('remove_vis', id, function(error, result) {
+            if (error) {
+                console.log("Error in deleting a visualisation:", error, "ID:", id._str);
+            } else {
+                console.log("An existing DAGs data vis was deleted with id:", result);
+                Router.go('home');
+            }
+        });
     }
 });
--- meteor/app/lib/routes.js
+++ meteor/app/lib/routes.js
@@ -10,3 +10,12 @@ Router.route('/', {
   controller: 'HomeController',
   where: 'client'
 });
+
+Router.route('vis/:_id', {
+    name: 'vis',
+    controller: 'VisController',
+    data: function(){
+        return DagsVis.findOne({ _id: this.params._id });
+    },
+    where: 'client'
+});
