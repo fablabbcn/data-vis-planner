06f9671d6373e9c19817ef19fdcfe4146efb3634
Massimo Menichinelli <info@openp2pdesign.org>
2017-10-26 14:36:03 +0200

    Testing the dagsvis collection

--- meteor/app/lib/collections/dags.js
+++ meteor/app/lib/collections/dags.js
@@ -69,23 +69,41 @@ if (Meteor.isServer) {
     });
 }
 
+// Check that there are no existing dagsvis for the same dag already existing
+existingDagsVis2 = DagsVis.find();
+console.log("DEBUG12": existingDagsVis2);
+
+// import { Dags } from '../lib/collections/dags.js';
+// import { DagsVis } from '../lib/collections/dags.js';
+
 // Listen for changes in the Airflow DAGs collections in order manage visualisations
 var cursor = Dags.find();
 cursor.observeChanges({
     added: function(id, fields) {
         // A new data source from DAGs was added
         console.log("A new DAGs data source was created in Mongo with id:", id._str);
-        // Create a visualisation
-        Meteor.call('add_vis', id, function(error, result) {
-            if (error) {
-                console.log("Error in adding a visualisation:", error, "ID:", id._str);
-            } else {
-                console.log("A new DAGs data vis was created with id:", result);
-                Router.go('vis', {
-                    _id: result
-                });
-            }
-        });
+        // Check that there are no existing dagsvis for the same dag already existing
+        existingDagsVis = DagsVis.find({
+            "dag_id": id._str
+        }, { fields: { "dag_id": 1 }});
+        existingDagsVis2 = DagsVis.find({}, { sort: { createdAt: -1 } });
+
+        console.log("DEBUG1": existingDagsVis);
+        console.log("DEBUG12": existingDagsVis2);
+        console.log("DEBUG2": existingDagsVis._docs._map);
+
+        if (_.isEmpty(existingDagsVis)) {
+            console.log(existingDagsVis);
+        } else {
+            // Create a visualisation
+            Meteor.call('add_vis', id, function(error, result) {
+                if (error) {
+                    console.log("Error in adding a visualisation:", error, "ID:", id._str);
+                } else {
+                    console.log("A new DAGs data vis was created with id:", result);
+                }
+            });
+        }
     },
     changed: function(id, fields) {
         // An existing data source from DAGs was edited
@@ -96,9 +114,6 @@ cursor.observeChanges({
                 console.log("Error in editing a visualisation:", error, "ID:", id._str);
             } else {
                 console.log("An existing DAGs data vis was edited with id:", result);
-                Router.go('vis', {
-                    _id: result
-                });
             }
         });
     },
@@ -111,7 +126,6 @@ cursor.observeChanges({
                 console.log("Error in deleting a visualisation:", error, "ID:", id._str);
             } else {
                 console.log("An existing DAGs data vis was deleted with id:", result);
-                Router.go('home');
             }
         });
     }
