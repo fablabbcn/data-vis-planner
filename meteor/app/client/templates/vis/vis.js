55ddb4620076cf97c3bbafc59d071e07ffb9a28d
Massimo Menichinelli <info@openp2pdesign.org>
2017-10-26 21:37:47 +0200

    Meteor: add waiting for date in route, fix first rough version of plotly barchart working

--- meteor/app/client/templates/vis/vis.js
+++ meteor/app/client/templates/vis/vis.js
@@ -1,35 +1,41 @@
 /*****************************************************************************/
 /* Vis: Event Handlers */
 /*****************************************************************************/
-Template.Vis.events({
-});
+Template.Vis.events({});
 
 /*****************************************************************************/
 /* Vis: Helpers */
 /*****************************************************************************/
 Template.Vis.helpers({
+    data: function() {
+        return this;
+    }
 });
 
 /*****************************************************************************/
 /* Vis: Lifecycle Hooks */
 /*****************************************************************************/
-Template.Vis.onCreated(function () {
-    Meteor.subscribe('dags');
-    Meteor.subscribe('dagsvis');
+Template.Vis.onCreated(function() {
+    this.subscribe("dagsvis");
 });
 
-Template.Vis.onRendered(function () {
+Template.Vis.onRendered(function() {
+
+    console.log("Data:", this.data);
+    var label = "number of labs";
+    var value = this.data.data["number of labs"];
+
     const data = [{
-      x: [1, 2, 3, 4, 5],
-      y: [1, 2, 4, 8, 16]
+        x: [label],
+        y: [value],
+        type: 'bar'
     }];
     const settings = {
-      margin: {
-        t: 0
-      }
+        margin: {
+            t: 0
+        }
     };
     Plotly.plot($('#visdiv')[0], data, settings);
 });
 
-Template.Vis.onDestroyed(function () {
-});
+Template.Vis.onDestroyed(function() {});
--- meteor/app/lib/routes.js
+++ meteor/app/lib/routes.js
@@ -1,20 +1,26 @@
 import { DagsVis } from './collections/dags.js'
 
 Router.configure({
-  layoutTemplate: 'MasterLayout',
-  loadingTemplate: 'Loading',
-  notFoundTemplate: 'NotFound'
+    layoutTemplate: 'MasterLayout',
+    loadingTemplate: 'Loading',
+    notFoundTemplate: 'NotFound'
 });
 
-
-Router.route('/', function () {
-  this.render('Home');
+Router.route('/', function() {
+    this.render('Home');
 });
 
-Router.route('/vis/:_id', function () {
-    this.render('Vis', {
-    data: function () {
-      return DagsVis.findOne({ _id: this.params._id });
+Router.route('/vis/:_id', function() {
+    this.wait(Meteor.subscribe('dagsvis', this.params._id));
+    if (this.ready()) {
+        this.render('Vis', {
+            data: function() {
+                return DagsVis.findOne({
+                    _id: this.params._id
+                });
+            }
+        });
+    } else {
+        this.render('Loading');
     }
-  });
 });
