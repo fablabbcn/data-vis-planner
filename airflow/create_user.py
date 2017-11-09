f99236ed22b2b640b821d560bef732eab11a6a6c
Massimo Menichinelli <info@openp2pdesign.org>
2017-11-09 18:07:43 +0100

    Fix airflow user creation by loading configuration from environment.env file for #10

--- airflow/create_user.py
+++ airflow/create_user.py
@@ -1,11 +1,20 @@
-import airflow
+# Create a user for Airflow
 from airflow import models, settings
 from airflow.contrib.auth.backends.password_auth import PasswordUser
 
+# Get configuration from the environment.env file
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '../environment.env')
+load_dotenv(dotenv_path)
+
+# Create the user
 user = PasswordUser(models.User())
-user.username = 'admin'
-user.email = 'admin@example.com'
-user.password = 'pass'
+user.username = os.environ.get("AIRFLOW_ADMIN")
+user.email = os.environ.get("AIRFLOW_EMAIL")
+user.password = os.environ.get("AIRFLOW_PASSWORD")
 session = settings.Session()
 
 try:
